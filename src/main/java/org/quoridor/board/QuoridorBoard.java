06b3dd35ca54836c669dd82257c9b047c3f5fb4b
Omar_Ayman <143454540+omarcs225@users.noreply.github.com>
2025-12-22 21:42:10 +0200

    fix and updata

--- src/main/java/org/quoridor/board/QuoridorBoard.java
+++ src/main/java/org/quoridor/board/QuoridorBoard.java
@@ -1,10 +1,10 @@
 
 package org.quoridor.board;
 
-import ai_project.board.model.Move;
-import ai_project.board.model.MoveKind;
-import ai_project.board.model.Pos;
-import ai_project.board.model.WallOrientation;
+import org.quoridor.board.model.Move;
+import org.quoridor.board.model.MoveKind;
+import org.quoridor.board.model.Pos;
+import org.quoridor.board.model.WallOrientation;
 import java.util.*;
 
 public final class QuoridorBoard implements Board {
